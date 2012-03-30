64f238898ce3dc5ca7acb796efb5da4adc34716d
mxsteini <mxsteini@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-30 08:43:44 +0000

    upload in TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ms_ajaxloader/trunk@60124 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ms_ajaxwrapper".
 #
-# Auto generated 29-03-2012 14:25
+# Auto generated 30-03-2012 10:12
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.4',
+	'version' => '0.0.5',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"678e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3168";s:14:"ext_tables.php";s:4:"f940";s:28:"ext_typoscript_constants.txt";s:4:"9011";s:24:"ext_typoscript_setup.txt";s:4:"5640";s:16:"locallang_db.xml";s:4:"cdb7";s:12:"t3jquery.txt";s:4:"a6b5";s:19:"doc/wizard_form.dat";s:4:"66a9";s:20:"doc/wizard_form.html";s:4:"f378";s:34:"pi1/class.tx_msajaxwrapper_pi1.php";s:4:"f497";s:19:"pi1/flexform_ds.xml";s:4:"7dfa";s:17:"pi1/locallang.xml";s:4:"7882";s:19:"res/ajax-loader.gif";s:4:"2a66";s:28:"res/ms_ajaxwrapper_loader.js";s:4:"f622";s:17:"res/template.html";s:4:"61a0";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"678e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"5d3d";s:12:"ext_icon.png";s:4:"ca04";s:17:"ext_localconf.php";s:4:"3168";s:14:"ext_tables.php";s:4:"fbc0";s:28:"ext_typoscript_constants.txt";s:4:"9011";s:24:"ext_typoscript_setup.txt";s:4:"5640";s:16:"locallang_db.xml";s:4:"cdb7";s:12:"t3jquery.txt";s:4:"a6b5";s:12:"doc/icon.svg";s:4:"ee62";s:14:"doc/manual.sxw";s:4:"74cb";s:34:"pi1/class.tx_msajaxwrapper_pi1.php";s:4:"f497";s:19:"pi1/flexform_ds.xml";s:4:"7dfa";s:17:"pi1/locallang.xml";s:4:"7882";s:19:"res/ajax-loader.gif";s:4:"2a66";s:28:"res/ms_ajaxwrapper_loader.js";s:4:"f622";s:17:"res/template.html";s:4:"6118";}',
 	'suggests' => array(
 	),
 );
